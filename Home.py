ffb40b667ad805a5480fe527573ae211a5db02a9
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-01 20:03:49 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -36,7 +36,7 @@ def get_videos(playlist_id):
     return response['items']
 
 def main():
-    centrar_texto("SamyTube Player")
+    centrar_texto("SamyTube Player 🙏", 2, 'purple')
     st.sidebar.title("Opciones")
 
     playlists = get_playlists()
