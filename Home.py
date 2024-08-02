d69828c22b8453b104700cc9cd12b7f246ab8e83
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-02 18:44:20 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,9 @@
+# Clave de la API de YouTube
+#API_KEY = st.secrets["youtube"]["api_key"]
+# Configuración de la página
+#st.set_page_config(page_title="YouTube Playlist Player", page_icon="❤")
+#channelId='UCpVi9NfcKzRmNyVFkbsq3lA'
+
 import streamlit as st
 from googleapiclient.discovery import build
 
@@ -5,21 +11,21 @@ from googleapiclient.discovery import build
 st.set_page_config(page_title="YouTube Playlist Player", page_icon="❤")
 
 # Clave de la API de YouTube
-API_KEY = st.secrets["youtube"]["api_key"]
+api_key = st.secrets["youtube"]["api_key"]
 
 # Crear el cliente de la API de YouTube
-youtube = build('youtube', 'v3', developerKey=API_KEY)
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 # Función para centrar el texto
 def centrar_texto(texto, tamanho, color):
     st.markdown(f"<h{tamanho} style='text-align: center; color: {color}'>{texto}</h{tamanho}>",
                 unsafe_allow_html=True)
 
-def get_playlists():
+def get_playlists(channel_id):
     # Recuperar listas de reproducción del canal
     request = youtube.playlists().list(
         part='snippet',
-        channelId='UCpVi9NfcKzRmNyVFkbsq3lA',
+        channelId=channel_id,
         maxResults=25
     )
     response = request.execute()
@@ -36,31 +42,50 @@ def get_videos(playlist_id):
     return response['items']
 
 def main():
-    centrar_texto("❤ ❤ SamyTube Player ❤ ❤ ", 2, 'pink')
-    st.sidebar.title("Opciones")
+    centrar_texto("❤ ❤ SamyTube Player ❤ ❤", 2, 'pink')
+    #st.sidebar.title("Opciones")
 
-    playlists = get_playlists()
-    playlist_titles = [playlist['snippet']['title'] for playlist in playlists]
-    selected_playlist = st.sidebar.selectbox("Selecciona una lista de reproducción", playlist_titles)
+    # Entradas de texto para la API key y el channel ID
+    #api_key = st.sidebar.text_input("Ingresa tu API Key", type="password")
+    channel_id = "UCpVi9NfcKzRmNyVFkbsq3lA"
 
-    if selected_playlist:
-        playlist_id = next(playlist['id'] for playlist in playlists if playlist['snippet']['title'] == selected_playlist)
-        videos = get_videos(playlist_id)
-        video_ids = [video['snippet']['resourceId']['videoId'] for video in videos]
+    if api_key and channel_id:
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        try:
+            playlists = get_playlists(channel_id)
+            playlist_titles = [playlist['snippet']['title'] for playlist in playlists]
+            selected_playlist = st.sidebar.selectbox("Selecciona una lista de reproducción", playlist_titles)
 
-        # Generar la lista de reproducción en formato JavaScript
-        playlist = ','.join(video_ids)
+            if selected_playlist:
+                playlist_id = next(playlist['id'] for playlist in playlists if playlist['snippet']['title'] == selected_playlist)
+                videos = get_videos(playlist_id)
+                video_ids = [video['snippet']['resourceId']['videoId'] for video in videos]
 
-        # Insertar el reproductor de YouTube con la lista de reproducción
-        # Insertar el reproductor de YouTube centrado
-        st.markdown(f"""
-        <div style="display: flex; justify-content: center;">
-            <iframe id="player" type="text/html" width="832" height="507"
-            src="https://www.youtube.com/embed/{video_ids[0]}?playlist={playlist}&autoplay=1&controls=1&loop=1"
-            frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        """, unsafe_allow_html=True)
+                # Mostrar miniaturas y títulos en la barra lateral
+                st.sidebar.markdown("### Videos en la lista de reproducción")
+                clicked_video_id = st.sidebar.radio(
+                    "Selecciona un video para reproducir",
+                    video_ids,
+                    format_func=lambda vid_id: next(video['snippet']['title'] for video in videos if video['snippet']['resourceId']['videoId'] == vid_id)
+                )
 
+                # Generar la lista de reproducción en formato JavaScript
+                playlist = ','.join(video_ids)
+
+                # Insertar el reproductor de YouTube centrado
+                st.markdown(f"""
+                <div style="display: flex; justify-content: center;">
+                    <iframe id="player" type="text/html" width="832" height="507"
+                    src="https://www.youtube.com/embed/{clicked_video_id}?playlist={playlist}&autoplay=1&controls=1&loop=1"
+                    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                """, unsafe_allow_html=True)
+            else:
+                st.warning("Por favor, selecciona una lista de reproducción.")
+        except Exception as e:
+            st.error(f"Error al recuperar datos de YouTube: {e}")
+    else:
+        st.warning("Por favor, ingresa tu API Key y Channel ID.")
 
 if __name__ == "__main__":
     main()
